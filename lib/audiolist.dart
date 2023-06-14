3140e767628c3a112c5d2acf361d9ef75286e2ab
hunaisp <hunaispc8@gmail.com>
2023-06-14 22:26:50 +0530

    first commit

---
+++ lib/audiolist.dart
@@ -0,0 +1,60 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+class AudioList extends StatefulWidget {
+  const AudioList({super.key});
+
+  @override
+  State<AudioList> createState() => _AudioListState();
+}
+
+class _AudioListState extends State<AudioList> {
+  final audioPlayer=AudioPlayer();
+  bool isPlaying=false;
+  Duration duration=Duration.zero;
+  Duration position=Duration.zero;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    audioPlayer.onPlayerStateChanged.listen((state) {
+      setState(() {
+        isPlaying=state==PlayerState.playing;
+      });
+    });
+
+    audioPlayer.onDurationChanged.listen((newDuration) {setState(() {
+      duration=newDuration;
+    }); });
+
+    audioPlayer.onPositionChanged.listen((newPosition) { setState(() {
+      position=newPosition;
+    });});
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    audioPlayer.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Column(children: [
+      Slider(min: 0,max: duration.inSeconds.toDouble(),value: position.inSeconds.toDouble(),onChanged: (value)async{
+        final position=Duration(seconds: value.toInt());
+        await audioPlayer.seek(position);
+        await audioPlayer.resume();
+      },),SizedBox(height: 40,),Row(children: [
+        // Text(formatTime(position)),Text(formatTime(duration-position))
+      ],),IconButton(onPressed: ()async{
+
+        if(isPlaying){
+          await audioPlayer.pause();
+        }else{
+          String url='';
+          await audioPlayer.play(url as Source);
+        }
+
+      }, icon:Icon(isPlaying?Icons.pause:Icons.play_arrow))
+    ],),);
+  }
+}
---
+++ lib/home.dart
@@ -0,0 +1,79 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_sound/flutter_sound.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final recorder = FlutterSoundRecorder();
+  bool isRecorderReady = false;
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initRecorder();
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    recorder.closeRecorder();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [StreamBuilder<RecordingDisposition>(builder: (context,snapshot){
+          final duration=snapshot.hasData?snapshot.data!.duration:Duration.zero;
+          String twoDigits(int n)=>n.toString().padLeft(10);
+          final twoDigitMinutes=twoDigits(duration.inMinutes.remainder(60));
+          final twoDigitSeconds=twoDigits(duration.inSeconds.remainder(60));
+          return Text('$twoDigitMinutes:$twoDigitSeconds');
+        },stream: recorder.onProgress,),
+          IconButton(
+              onPressed: ()async {
+                if(recorder.isRecording){
+                  await stop();
+                }
+                else{
+                  await record();
+                }
+                setState(() {
+
+                });
+              },
+              icon: Icon(recorder.isRecording ? Icons.pause : Icons.mic))
+        ],
+      ),
+    );
+  }
+
+  Future record()async{
+    if(!isRecorderReady)return;
+    await recorder.startRecorder(toFile: 'audio');
+  }
+  Future stop()async{
+    if(!isRecorderReady)return;
+   final path= await recorder.stopRecorder();
+    final audioFile=File(path!);
+    print('Recorded audio:$audioFile');
+  }
+
+  Future initRecorder()async{
+  final status=await Permission.microphone.request();
+  if(status!=PermissionStatus.granted){
+    throw 'micro phone permission not granted';
+  }
+  await recorder.openRecorder();
+  isRecorderReady=true;
+  }
+
+
+}
